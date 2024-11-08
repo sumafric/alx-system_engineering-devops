e985b39ca751705ba86e719bf3855a175c64301c
Abel Sum <abelsum1330@gmail.com>
2024-11-08 14:50:26 -0800

    Repetition Token #0

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-#Ruby script that accepts one argument and pass it to a regular expression matching method
+#Ruby script matches a given pattern
 puts ARGV[0].scan(/hbt{2,5}n/).join
