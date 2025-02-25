21cda776a5003267e5eab19bbb0c53342389cfc5
sumafric <abelsum1330@gmail.com>
2025-02-25 10:54:22 -0800

    How many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    dict = response.json()
+    if 'data' not in dict:
+        return 0
+    if 'subscribers' not in dict.get('data'):
+        return 0
+    return response.json()['data']['subscribers']
